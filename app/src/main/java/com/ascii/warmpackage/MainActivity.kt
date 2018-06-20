f77fbe6a146c849d431f17a1942280ca22d8cdc4
Ascii Huang <asciihuang@kkbox.com>
2018-06-20 12:00:01 +0800

    Revert code

--- app/src/main/java/com/ascii/warmpackage/MainActivity.kt
+++ app/src/main/java/com/ascii/warmpackage/MainActivity.kt
@@ -150,7 +150,11 @@ class MainActivity : AppCompatActivity(), WarmPackageView {
     private fun createSnackBar(): Unit {
         // object: View.OnClickListener 是 new View.OnClickListener 最直接的翻譯
         snackbar = Snackbar.make(parentView!!, R.string.running, Snackbar.LENGTH_INDEFINITE)
-                .setAction(R.string.action_exit) { presenter?.stopWarm() }
+                .setAction(R.string.action_exit, object : View.OnClickListener {
+                    override fun onClick(v: View?) {
+                        presenter?.stopWarm()
+                    }
+                })
         snackbar?.let {
             it.setActionTextColor(Color.WHITE)
             it.show()
